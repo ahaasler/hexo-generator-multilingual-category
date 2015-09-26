ef6a7a8663d7e51753bc31742872f2451f80c633
Adrian Haasler García <dev@adrianhaasler.com>
2015-09-26 15:46:16 +0000

    Add preversion and postversion npm scripts
    This will automagically test before making a version and then push both
    the commit and the tag upon confirmation.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var gulp = require('gulp');
+var shell = require('gulp-shell');
+var inquirer = require('inquirer');
 var $ = require('gulp-load-plugins')();
 var rirmaf = require('rimraf');
 
@@ -37,3 +39,28 @@ gulp.task('watch', function(){
 });
 
 gulp.task('test', ['mocha', 'jshint']);
+
+gulp.task('git-show', shell.task([
+  'git show -1'
+]));
+
+gulp.task('git-push', shell.task([
+  'git push',
+  'git push --tags'
+]));
+
+gulp.task('git-push:confirm', ['git-show'], function(done) {
+  inquirer.prompt([{
+    type: 'confirm',
+    message: 'Push version?',
+    default: false,
+    name: 'push'
+  }], function(answers) {
+    if(answers.push) {
+      gulp.start('git-push');
+    }
+    done();
+  });
+});
+
+gulp.task('release', ['git-push:confirm']);
