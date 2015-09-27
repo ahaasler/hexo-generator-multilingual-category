70498d7439204846d3364a837f599c9dda03f2a1
Adrian Haasler García <dev@adrianhaasler.com>
2015-09-27 14:56:02 +0000

    Add multilingual behavior
    For this generator to work the Hexo configuration must specify more
    than one language, posts should have alternate language versions and
    categories should be easy to infer. It's best if there's at least one
    post in each category that only has that one category listed in its
    categories variable. It can also work in other complex scenarios where
    all posts have multiple categories, but not all share the same
    alternate categories.
    
    This is part of #1.

--- test/index.js
+++ test/index.js
@@ -3,80 +3,98 @@
 var should = require('chai').should();
 var Hexo = require('hexo');
 
-describe('Category generator', function(){
-  var hexo = new Hexo(__dirname, {silent: true});
+describe('Category generator', function() {
+  var hexo = new Hexo(__dirname, {
+    silent: true
+  });
   var Post = hexo.model('Post');
   var generator = require('../lib/generator').bind(hexo);
   var posts;
   var locals;
 
   // Default config
+  hexo.config.language = ['en', 'es'];
   hexo.config.category_generator = {
     per_page: 10
   };
 
-  before(function(){
-    return Post.insert([
-      {source: 'foo', slug: 'foo', date: 1e8},
-      {source: 'bar', slug: 'bar', date: 1e8 + 1},
-      {source: 'baz', slug: 'baz', date: 1e8 - 1},
-      {source: 'boo', slug: 'boo', date: 1e8 + 2}
-    ]).then(function(data){
+  before(function() {
+    return Post.insert([{
+      source: 'foo',
+      slug: 'foo',
+      date: 1e8,
+      lang: 'en'
+    }, {
+      source: 'bar',
+      slug: 'bar',
+      date: 1e8 + 1,
+      lang: 'en'
+    }, {
+      source: 'baz',
+      slug: 'baz',
+      date: 1e8 - 1,
+      lang: 'en'
+    }, {
+      source: 'boo',
+      slug: 'boo',
+      date: 1e8 + 2,
+      lang: 'en'
+    }]).then(function(data) {
       posts = data;
 
-      return posts[0].setCategories(['foo']).then(function(){
+      return posts[0].setCategories(['foo']).then(function() {
         return posts[1].setCategories(['bar']);
-      }).then(function(){
+      }).then(function() {
         return posts[2].setCategories(['foo']);
-      }).then(function(){
+      }).then(function() {
         return posts[3].setCategories(['foo']);
       });
-    }).then(function(){
+    }).then(function() {
       locals = hexo.locals.toObject();
     });
   });
 
-  it('pagination enabled', function(){
+  it('pagination enabled', function() {
     hexo.config.category_generator.per_page = 2;
 
     var result = generator(locals);
 
     result.length.should.eql(3);
 
-    for (var i = 0, len = result.length; i < len; i++){
+    for (var i = 0, len = result.length; i < len; i++) {
       result[i].layout.should.eql(['category', 'archive', 'index']);
     }
 
-    result[0].path.should.eql('categories/foo/');
-    result[0].data.base.should.eql('categories/foo/');
+    result[0].path.should.eql('en/categories/foo/');
+    result[0].data.base.should.eql('en/categories/foo/');
     result[0].data.total.should.eql(2);
     result[0].data.current.should.eql(1);
-    result[0].data.current_url.should.eql('categories/foo/');
+    result[0].data.current_url.should.eql('en/categories/foo/');
     result[0].data.posts.eq(0)._id.should.eql(posts[3]._id);
     result[0].data.posts.eq(1)._id.should.eql(posts[0]._id);
     result[0].data.prev.should.eql(0);
     result[0].data.prev_link.should.eql('');
     result[0].data.next.should.eql(2);
-    result[0].data.next_link.should.eql('categories/foo/page/2/');
+    result[0].data.next_link.should.eql('en/categories/foo/page/2/');
     result[0].data.category.should.eql('foo');
 
-    result[1].path.should.eql('categories/foo/page/2/');
-    result[1].data.base.should.eql('categories/foo/');
+    result[1].path.should.eql('en/categories/foo/page/2/');
+    result[1].data.base.should.eql('en/categories/foo/');
     result[1].data.total.should.eql(2);
     result[1].data.current.should.eql(2);
-    result[1].data.current_url.should.eql('categories/foo/page/2/');
+    result[1].data.current_url.should.eql('en/categories/foo/page/2/');
     result[1].data.posts.eq(0)._id.should.eql(posts[2]._id);
     result[1].data.prev.should.eql(1);
-    result[1].data.prev_link.should.eql('categories/foo/');
+    result[1].data.prev_link.should.eql('en/categories/foo/');
     result[1].data.next.should.eql(0);
     result[1].data.next_link.should.eql('');
     result[1].data.category.should.eql('foo');
 
-    result[2].path.should.eql('categories/bar/');
-    result[2].data.base.should.eql('categories/bar/');
+    result[2].path.should.eql('en/categories/bar/');
+    result[2].data.base.should.eql('en/categories/bar/');
     result[2].data.total.should.eql(1);
     result[2].data.current.should.eql(1);
-    result[2].data.current_url.should.eql('categories/bar/');
+    result[2].data.current_url.should.eql('en/categories/bar/');
     result[2].data.posts.eq(0)._id.should.eql(posts[1]._id);
     result[2].data.prev.should.eql(0);
     result[2].data.prev_link.should.eql('');
@@ -88,22 +106,24 @@ describe('Category generator', function(){
     hexo.config.category_generator.per_page = 10;
   });
 
-  it('pagination disabled', function(){
+  it('pagination disabled', function() {
     hexo.config.category_generator.per_page = 0;
 
     var result = generator(locals);
 
+    console.log(result);
+
     result.length.should.eql(2);
 
-    for (var i = 0, len = result.length; i < len; i++){
+    for (var i = 0, len = result.length; i < len; i++) {
       result[i].layout.should.eql(['category', 'archive', 'index']);
     }
 
-    result[0].path.should.eql('categories/foo/');
-    result[0].data.base.should.eql('categories/foo/');
+    result[0].path.should.eql('en/categories/foo/');
+    result[0].data.base.should.eql('en/categories/foo/');
     result[0].data.total.should.eql(1);
     result[0].data.current.should.eql(1);
-    result[0].data.current_url.should.eql('categories/foo/');
+    result[0].data.current_url.should.eql('en/categories/foo/');
     result[0].data.posts.eq(0)._id.should.eql(posts[3]._id);
     result[0].data.posts.eq(1)._id.should.eql(posts[0]._id);
     result[0].data.posts.eq(2)._id.should.eql(posts[2]._id);
@@ -113,11 +133,11 @@ describe('Category generator', function(){
     result[0].data.next_link.should.eql('');
     result[0].data.category.should.eql('foo');
 
-    result[1].path.should.eql('categories/bar/');
-    result[1].data.base.should.eql('categories/bar/');
+    result[1].path.should.eql('en/categories/bar/');
+    result[1].data.base.should.eql('en/categories/bar/');
     result[1].data.total.should.eql(1);
     result[1].data.current.should.eql(1);
-    result[1].data.current_url.should.eql('categories/bar/');
+    result[1].data.current_url.should.eql('en/categories/bar/');
     result[1].data.posts.eq(0)._id.should.eql(posts[1]._id);
     result[1].data.prev.should.eql(0);
     result[1].data.prev_link.should.eql('');
@@ -129,15 +149,15 @@ describe('Category generator', function(){
     hexo.config.category_generator.per_page = 10;
   });
 
-  it('custom pagination_dir', function(){
+  it('custom pagination_dir', function() {
     hexo.config.category_generator.per_page = 2;
     hexo.config.pagination_dir = 'yo';
 
     var result = generator(locals);
 
-    result.map(function(item){
+    result.map(function(item) {
       return item.path;
-    }).should.eql(['categories/foo/', 'categories/foo/yo/2/', 'categories/bar/']);
+    }).should.eql(['en/categories/foo/', 'en/categories/foo/yo/2/', 'en/categories/bar/']);
 
     // Restore config
     hexo.config.category_generator.per_page = 10;
